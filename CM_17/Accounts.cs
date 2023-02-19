db513268a28bd1aaefaf329f2fc20153e63703cf
Солнце <@LAPTOP-DPKNJGCI>
2023-02-19 06:22:11 +0300

    Добавьте файлы проекта.

---
+++ CM_17/Accounts.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CM_17
+{
+    public class Accounts<T>
+    {
+        public T Num { get; set; }
+        public double Balance { get; set; }
+        public string Name { get; set; }
+
+        //public Accounts(T num, double balance, string name)
+        //{
+        //    Num = num;
+        //    Balance = balance;
+        //    Name = name;
+        //}
+        public string GetInfo()
+        {
+            return $"{ Num} {Balance} { Name}";
+        }
+        public void Input()
+        {
+            Console.WriteLine("Введите номер");
+            string stringNum = Console.ReadLine();
+            Num = (T)Convert.ChangeType(stringNum, typeof(T));
+
+            Console.WriteLine("Введите баланс");
+            Balance = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите ФИО");
+            Name = Console.ReadLine();
+        }
+    }
+}
---
+++ CM_17/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CM_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Введите числовой номер");
+            //int numInt = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Введите баланс");
+            //double balance = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Введите Имя");
+            //string name = Console.ReadLine();
+            //Accounts<int> accounts1 = new Accounts<int>(numInt,balance,name);
+            //Console.WriteLine(accounts1.GetInfo());
+
+            //Console.WriteLine("Введите строковый номер");
+            //string numString = Console.ReadLine();
+            //Console.WriteLine("Введите баланс");
+            //balance = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Введите Имя");
+            //name = Console.ReadLine();
+            //Accounts<string> accounts2 = new Accounts<string>(numstring,balance,name);
+            //Console.WriteLine(accounts2.GetInfo())
+
+            Accounts<int> accounts1 = new Accounts<int>();
+            accounts1.Input();
+            Console.WriteLine(accounts1.GetInfo());
+
+            Accounts<string> accounts2 = new Accounts<string>();
+            accounts2.Input();
+            Console.WriteLine(accounts2.GetInfo());
+            Console.ReadKey();
+        }
+    }
+}
